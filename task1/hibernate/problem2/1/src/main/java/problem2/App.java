00deb28c5e33a0560bf0624ee46708a6e6e0041d
pritys1 <111412815+pritys1@users.noreply.github.com>
2022-08-24 02:46:46 +0530

    add

---
+++ task1/hibernate/problem2/1/src/main/java/problem2/App.java
@@ -0,0 +1,14 @@
+package problem2;
+package ;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
